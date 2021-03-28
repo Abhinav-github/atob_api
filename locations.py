d61549872145ad74d9220ffea9483c25c5660b04
Abhinav <singh.avi99@gmail.com>
2021-03-28 15:21:51 -0500

    fixed database issues

--- locations.py
+++ locations.py
@@ -1,10 +1,14 @@
 import csv
+from csv import writer
+import os
 from ping import *
 def write_csv(trucks):
-    titles = ('Latitude', 'Longitude')
+    titles = ('ID', 'Latitude', 'Longitude')
     with open('locations.csv', 'w') as locations:
-        locations.write("%s,%s\n"%(titles))
+        writer_object = writer(locations)
+        writer_object.writerow(titles)
         for key in trucks.keys():
             if trucks[key] != (0.0, 0.0):
-                locations.write("%s,%s\n"%(trucks[key]))
+                writer_object.writerow((key, trucks[key][0], trucks[key][1]))
+        locations.close()
     print('test2')
